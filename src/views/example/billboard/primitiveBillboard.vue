f1f0ebd91ee2858475f46ed52bbb101ae8930551
anxueyu <1358042645@qq.com>
2025-02-21 20:53:14 +0800

    fix: 修复问题

--- src/views/example/billboard/primitiveBillboard.vue
+++ src/views/example/billboard/primitiveBillboard.vue
@@ -1,175 +1,45 @@
-<script setup>
-import { onUnmounted, onMounted } from "vue";
-import 'cesium/Source/Widgets/widgets.css';
-import markList from '@/assets/images/marker/index'
+<template>
+  <div id="cesiumContainer" class="fullSize"></div>
+  <div class="operation">
+    <span>随机生成点位</span>
+    <el-input-number :min="1" v-model="pointNum" :step="100"></el-input-number>
+    <el-button type="primary" @click="">生成</el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import * as Cesium from "cesium";
-import PrimitiveCluster from "@/modules/cesium/PrimitiveCluster.js";
+import markList from '@/assets/images/marker/index'
+import 'cesium/Source/Widgets/widgets.css';
 
-let viewer;
-let billboardsCollectionCombine = new Cesium.BillboardCollection();
-
-let primitivesCollection;
-let primitives = null;
-
-const onCluster = () => {
-  primitivesCollection = new Cesium.PrimitiveCollection();
-  billboardsCollectionCombine = new Cesium.BillboardCollection();
-  const primitivecluster = new PrimitiveCluster();
-  primitivecluster.enabled = true;
-  primitivecluster.pixelRange = 60;
-  primitivecluster.minimumClusterSize = 2;
-
-  for (let i = 0; i < 10000; i++) {
+var viewer: Cesium.Viewer;
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyMjBkODk3NS0xZmE4LTQ5MzgtYTAxZC1mZTZhZTVmMTY3ZjQiLCJpZCI6MTcwNzE3LCJpYXQiOjE2OTY4MTY5OTN9.YivsBCkT8fHJNB5lFMFo2bh7860luv368ALHw-_gCD0";
+const pointNum = ref(10000);
+const createBillboards = () => {
+  const billboardCollection = new Cesium.BillboardCollection()
+  for (let i = 0; i < pointNum.value; i++) {
     const longitude = Math.random() * (110 - 90) + 90;
     const latitude = Math.random() * (40 - 30) + 30;
-    billboardsCollectionCombine.add({
-      position: Cesium.Cartesian3.fromDegrees(longitude, latitude, 100),
+    billboardCollection.add({
+      position: Cesium.Cartesian3.fromDegrees(longitude, latitude, 10),
       image: markList.LaceRed,
-      width: 32,
-      height: 32,
+      scale: 0.5,
+      disableDepthTestDistance: 10,
+      verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
     });
   }
-  viewer.scene.primitives.add(billboardsCollectionCombine)
-  primitivecluster._billboardCollection = billboardsCollectionCombine;
-  primitivecluster._initialize(viewer.scene);
-
-  primitivesCollection.add(primitivecluster);
-  primitives = viewer.scene.primitives.add(primitivesCollection);
-
-  primitivecluster.clusterEvent.addEventListener(
-    (clusteredEntities, cluster) => {
-      console.log(clusteredEntities.length, cluster);
-      // cluster.label.show = false;
-      // cluster.billboard.show = true;
-      // cluster.billboard.verticalOrigin = Cesium.VerticalOrigin.BOTTOM;
-
-      // // 根据聚合数量的多少设置不同层级的图片以及大小
-      // combineIconAndLabel(markList.M1, clusteredEntities.length, 64)
-      //   .then(dataUrl => {
-      //     cluster.billboard.image = dataUrl;
-      //     cluster.billboard.width = 40;
-      //     cluster.billboard.height = 40;
-      //   })
-      //   .catch(error => {
-      //     console.error('Error combining icon and label:', error);
-      //   });
-    }
-  );
+  viewer.scene.primitives.add(billboardCollection)
 };
 
-async function combineIconAndLabel(url, label, size) {
-  // 创建画布对象
-  const canvas = document.createElement("canvas");
-  canvas.width = size;
-  canvas.height = size;
-  const ctx = canvas.getContext("2d");
-
-  if (!ctx) {
-    throw new Error('Could not get 2D context');
-  }
-
-  try {
-    const resource = new Cesium.Resource({ url });
-    const image = await resource.fetchImage();
-    if (image)
-      ctx.drawImage(image, 0, 0);
-  } catch (e) {
-    console.error('Failed to fetch or draw image:', e);
-    throw e;
-  }
-
-  // 渲染字体
-  ctx.fillStyle = Cesium.Color.BLACK.toCssColorString();
-  ctx.font = "bold 20px Microsoft YaHei";
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
-  ctx.fillText(String(label), size / 2, size / 2);
-
-  return canvas.toDataURL(); // 将画布转换为 Data URL
-}
-
-const onClear = () => {
-  primitives?.destroy();
-  primitivesCollection.destroy();
-  billboardsCollectionCombine.destroy();
-  primitives = null;
-  primitivesCollection = new Cesium.PrimitiveCollection();
-  billboardsCollectionCombine = new Cesium.BillboardCollection();
-};
-let primitivesCluster;
-const addCluster = () => {
-  // 使用primitives 添加点
-  var labels = new Cesium.LabelCollection()
-  var billboards = new Cesium.BillboardCollection()
-  var collection = new Cesium.PrimitiveCollection()
-  for (let i = 0; i < 10000; i++) {
-    const longitude = Math.random() * (110 - 90) + 90;
-    const latitude = Math.random() * (40 - 30) + 30;
-    let title = {
-      id: longitude,
-      position: Cesium.Cartesian3.fromDegrees(longitude, latitude, 10),
-      text: i,
-      font: "30px Source Han Sans CN", //字体样式
-      fillColor: new Cesium.Color.fromCssColorString("#ffffff"), //字体颜色
-      showBackground: true, //是否显示背景颜色
-      backgroundColor: new Cesium.Color.fromCssColorString("#000000"), //背景颜色
-      verticalOrigin: Cesium.VerticalOrigin.BOTTOM, //垂直位置
-      horizontalOrigin: Cesium.HorizontalOrigin.CENTER, //水平位置
-    }
-    let img = {
-      id: longitude,
-      position: Cesium.Cartesian3.fromDegrees(longitude, latitude, 10),
-      image: markList.LaceRed,
-      scale: 1,
-      verticalOrigin: Cesium.VerticalOrigin.TOP, //垂直位置
-    }
-    // labels.add(title)
-    billboards.add(img)
-  }
-  let primitivecluster = null
-  primitivecluster = new PrimitiveCluster()
-  primitivecluster.enabled = true
-  primitivecluster.pixelRange = 1
-  primitivecluster.minimumClusterSize = 10
-  primitivecluster._billboardCollection = billboards
-  // 同时在赋值时调用_initialize方法
-  primitivecluster._initialize(viewer.scene)
-  collection.add(primitivecluster)
-  primitivesCluster = viewer.scene.primitives.add(collection)
-  const pinBuilder = new Cesium.PinBuilder()
-  primitivecluster.clusterEvent.addEventListener((clusteredEntities, cluster) => {
-    // 关闭自带的显示聚合数量的标签
-    cluster.label.show = false
-    cluster.billboard.show = true
-    cluster.billboard.verticalOrigin = Cesium.VerticalOrigin.BOTTOM
-    let pinImg = pinBuilder.fromText(cluster.label.text, Cesium.Color.RED, 60).toDataURL()
-    // 根据聚合数量的多少设置不同层级的图片以及大小
-    cluster.billboard.image = pinImg
-  })
-  return primitivecluster
-}
 onMounted(() => {
   viewer = new Cesium.Viewer("cesiumContainer", {
     // terrain: Cesium.Terrain.fromWorldTerrain(),
   });
-  viewer.camera.setView({
-    destination: Cesium.Cartesian3.fromDegrees(120.36, 36.09, 40000),
-  });
-});
+  createBillboards();
 
-onUnmounted(() => {
-  onClear();
 });
 </script>
-
-<template>
-  <div id="cesiumContainer" class="fullSize"></div>
-
-  <div class="operation">
-    <el-button type="primary" @click="addCluster">primitive打点聚合</el-button>
-    <el-button type="primary" @click="onClear">清除打点</el-button>
-  </div>
-</template>
 
 <style scoped>
 .fullSize {
@@ -179,8 +49,7 @@ onUnmounted(() => {
   padding: 0;
   overflow: hidden;
 }
-
-.operation {
+.operation{
   position: fixed;
   top: 20px;
   left: 20px;
--- src/views/example/billboard/primitiveCluster.vue
+++ src/views/example/billboard/primitiveCluster.vue
@@ -3,44 +3,109 @@
   <div class="operation">
     <span>随机生成点位</span>
     <el-input-number :min="1" v-model="pointNum" :step="100"></el-input-number>
-    <el-button type="primary" @click="">生成</el-button>
+    <el-button type="primary" @click="generatePoints">生成</el-button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import * as Cesium from "cesium";
 import markList from '@/assets/images/marker/index'
 import 'cesium/Source/Widgets/widgets.css';
+import PrimitiveCluster from "@/modules/cesium/PrimitiveCluster.js"
 
 var viewer: Cesium.Viewer;
-Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyMjBkODk3NS0xZmE4LTQ5MzgtYTAxZC1mZTZhZTVmMTY3ZjQiLCJpZCI6MTcwNzE3LCJpYXQiOjE2OTY4MTY5OTN9.YivsBCkT8fHJNB5lFMFo2bh7860luv368ALHw-_gCD0";
+var billboards: Cesium.BillboardCollection;
 const pointNum = ref(10000);
+const pixelRange = 40; // 聚合范围（像素）
+
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyMjBkODk3NS0xZmE4LTQ5MzgtYTAxZC1mZTZhZTVmMTY3ZjQiLCJpZCI6MTcwNzE3LCJpYXQiOjE2OTY4MTY5OTN9.YivsBCkT8fHJNB5lFMFo2bh7860luv368ALHw-_gCD0";
+
 const createBillboards = () => {
-  const billboardCollection = new Cesium.BillboardCollection()
+  let data = [];
   for (let i = 0; i < pointNum.value; i++) {
     const longitude = Math.random() * (110 - 90) + 90;
     const latitude = Math.random() * (40 - 30) + 30;
-    billboardCollection.add({
-      position: Cesium.Cartesian3.fromDegrees(longitude, latitude, 10),
-      image: markList.LaceRed,
-      scale: 0.5,
-      disableDepthTestDistance: 10,
-      verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+    data.push({
+      longitude, latitude
     });
   }
-  viewer.scene.primitives.add(billboardCollection)
+  addCluster(data);
+};
+let primitivesCluster: any;
+const addCluster = (data)=> {
+  // 使用primitives 添加点
+  var labels = new Cesium.LabelCollection()
+  var billboards = new Cesium.BillboardCollection()
+  var collection = new Cesium.PrimitiveCollection()
+
+  data.forEach(ele => {
+    let center = {
+      lng: ele.longitude,
+      lat: ele.latitude,
+    }
+    let title = {
+      id: ele.id,
+      position: Cesium.Cartesian3.fromDegrees(Number(center.lng), Number(center.lat), 0),
+      text: ele.name,
+      font: "30px Source Han Sans CN", //字体样式
+      fillColor: new Cesium.Color.fromCssColorString("#ffffff"), //字体颜色
+      showBackground: true, //是否显示背景颜色
+      backgroundColor: new Cesium.Color.fromCssColorString("#000000"), //背景颜色
+      verticalOrigin: Cesium.VerticalOrigin.BOTTOM, //垂直位置
+      horizontalOrigin: Cesium.HorizontalOrigin.CENTER, //水平位置
+    }
+    let img = {
+      id: ele.id,
+      position: Cesium.Cartesian3.fromDegrees(Number(center.lng), Number(center.lat), 80),
+      image: markList.LaceRed,
+      width: ele.poiWid,
+      height: ele.poiHig,
+      scale: 1,
+      verticalOrigin: Cesium.VerticalOrigin.TOP, //垂直位置
+    }
+    labels.add(title)
+    billboards.add(img)
+  })
+  let primitivecluster = null
+  primitivecluster = new PrimitiveCluster()
+  primitivecluster.enabled = true
+  primitivecluster.pixelRange = 1
+  primitivecluster.minimumClusterSize = 10
+  primitivecluster._billboardCollection = billboards
+  // 同时在赋值时调用_initialize方法
+  primitivecluster._initialize(viewer.scene)
+  collection.add(primitivecluster)
+  primitivesCluster = viewer.scene.primitives.add(collection)
+  const pinBuilder = new Cesium.PinBuilder()
+  primitivecluster.clusterEvent.addEventListener((clusteredEntities, cluster) => {
+    // 关闭自带的显示聚合数量的标签
+    cluster.label.show = false
+    cluster.billboard.show = true
+    cluster.billboard.verticalOrigin = Cesium.VerticalOrigin.BOTTOM
+    let pinImg = pinBuilder.fromText(cluster.label.text, Cesium.Color.RED, 60).toDataURL()
+    // 根据聚合数量的多少设置不同层级的图片以及大小
+    cluster.billboard.image = pinImg
+  })
+  return primitivecluster
+}
+
+const generatePoints = () => {
+  viewer.scene.primitives.removeAll();
+  createBillboards();
 };
 
 onMounted(() => {
   viewer = new Cesium.Viewer("cesiumContainer", {
     // terrain: Cesium.Terrain.fromWorldTerrain(),
   });
-  createBillboards();
+  alert("当前cesium版本 1.124 版本过高，该聚合方法不可用，经测试可在 1.105 版本可用");
+});
 
+watch(pointNum, () => {
+  generatePoints();
 });
 </script>
-
 <style scoped>
 .fullSize {
   width: 100%;
@@ -49,7 +114,8 @@ onMounted(() => {
   padding: 0;
   overflow: hidden;
 }
-.operation{
+
+.operation {
   position: fixed;
   top: 20px;
   left: 20px;
